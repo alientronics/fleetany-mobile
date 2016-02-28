49ccbf7cd4a18821cff09a893b6000af641211e9
Henrique Santos <henrique@Henriques-Mac-mini.local>
2016-02-28 10:39:59 -0300

    add geolocation #4

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -1,7 +1,20 @@
 import {Page} from 'ionic-framework/ionic';
+import {Geolocation} from 'ionic-native';
 
 
 @Page({
   templateUrl: 'build/pages/page2/page2.html'
 })
-export class Page2 {}
+export class Page2 {
+
+  constructor() {
+
+	let watch = Geolocation.watchPosition();
+	watch.subscribe((data) => {
+		this.latitude = data.coords.latitude;
+		this.longitude = data.coords.longitude;
+	})
+
+  }
+
+}
