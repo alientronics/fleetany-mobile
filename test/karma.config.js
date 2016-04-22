2818028b5404e4e4cc86695962f050f7449a3e99
Henrique Santos <henrique@Henriques-Mac-mini.local>
2016-04-22 00:45:45 -0300

    fix tests

--- test/karma.config.js
+++ test/karma.config.js
@@ -14,9 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/zone.js/dist/zone-microtask.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
       'node_modules/es6-module-loader/dist/es6-module-loader.js',
       'node_modules/traceur/bin/traceur.js',
       'node_modules/systemjs/dist/system.src.js',
