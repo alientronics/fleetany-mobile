b7a24401258cf1af3a8ce500cb8ec6f3d7ed1b8a
Rafael <rafaelpviegas@gmail.com>
2016-04-22 22:15:26 -0300

    #5 Mensagem de aviso em tentativo de envio de dados sem a escolha de veículo

--- app/pages/page3/page3.ts
+++ app/pages/page3/page3.ts
@@ -18,11 +18,13 @@ export class Page3 {
   private miliage: AbstractControl;
   private tankfill: AbstractControl;
   private fuelsent: boolean;
+  private vehiclefailed: boolean;
   private types: Array<any>;
 
   constructor(fb: FormBuilder, userData: UserData) {
     this.userData = userData;
   	this.fuelsent = false;
+  	this.vehiclefailed = false;
     this.types = []
 
     this.fuelForm = fb.group({  
@@ -55,24 +57,30 @@ export class Page3 {
           params.end_mileage = value.miliage;
           params.fuel_type = 1;
           params.tank_fill_up = value.tankfill ? 1 : 0;			
+           
+           
+          if (!this.plate) {
+          	vehiclefailed = true;
+          } else {
+            this.userData.postApi('trip', params).subscribe(res => {
+
+                for(let field in this.fuelForm.controls) {
+                  this.fuelForm.controls[field].updateValue('');
+                };
+
+                this.fuelForm.controls['type'].updateValue('regular');
+                this.fuelForm.controls['tankfill'].updateValue(true);
+
+                this.fuelsent = true;
             
-          this.userData.postApi('trip', params).subscribe(res => {
-
-            for(let field in this.fuelForm.controls) {
-                this.fuelForm.controls[field].updateValue('');
-            };
-
-            this.fuelForm.controls['type'].updateValue('regular');
-            this.fuelForm.controls['tankfill'].updateValue(true);
-
-            this.fuelsent = true;
-            
-          });
+            });
+          }
       }
   } 
 
   closeAlert() {
   	this.fuelsent = false;
+  	this.vehiclefailed = false;
   }
 
 }
