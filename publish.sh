75649ebf051e59b311a1e983e93255469f69cb1d
Henrique Dias <heukirne@gmail.com>
2016-07-23 16:19:54 -0300

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 rm fleetany-mobile-armv7.apk
 rm fleetany-mobile-x86.apk
 cordova build --release android
-jarsigner -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore fleetany.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk fleetany-key
+jarsigner -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore fleetany.keystore platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk fleetany-key
 ~/Library/Android/sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk fleetany-mobile-armv7.apk
+jarsigner -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore fleetany.keystore platforms/android/build/outputs/apk/android-x86-release-unsigned.apk.apk fleetany-key
 ~/Library/Android/sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk fleetany-mobile-x86.apk
