2c3bd1627203756415106237597ed5b527289b00
Henrique Dias <heukirne@gmail.com>
2016-07-23 15:53:41 -0300

    publish new version

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
-rm fleetany-mobile.apk
+rm fleetany-mobile-armv7.apk
+rm fleetany-mobile-x86.apk
 cordova build --release android
 jarsigner -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore fleetany.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk fleetany-key
-~/Library/Android/sdk/build-tools/23.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk fleetany-mobile.apk
+~/Library/Android/sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk fleetany-mobile-armv7.apk
+~/Library/Android/sdk/build-tools/23.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-x86-release-unsigned.apk fleetany-mobile-x86.apk
