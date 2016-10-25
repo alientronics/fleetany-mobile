e6c736e0b01e856848bc74dbfc480deab848145b
Henrique Dias <heukirne@gmail.com>
2016-10-25 09:08:25 -0200

    update to ionic rc1 [ci-skip]

--- app/config/settings.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export class Settings {
-    public base_path: string = 'http://localhost:8000/api/v1/'
-    public api_token: string = 'OTscjZ19F';
-}
---
+++ src/app/config/settings.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+export class Settings {
+    public base_path: string = 'https://gemeos1.websiteseguro.com/api/v1/'
+    public api_token: string = 'yoZrs2nZPJiwdEs1j';
+}
