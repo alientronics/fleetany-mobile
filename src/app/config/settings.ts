b4b08b4e1338cd4c08502b25b80bdafb72578b13
Henrique Dias <heukirne@gmail.com>
2016-11-05 08:37:16 -0200

    change tests

--- src/app/config/settings.ts
+++ src/app/config/settings.ts
@@ -1,6 +1,6 @@
 'use strict';
 
 export class Settings {
-    public base_path: string = 'https://gemeos1.websiteseguro.com/api/v1/'
-    public api_token: string = 'yoZrs2nZPJiwdEs1j';
+    public base_path: string = 'http://localhost:8000/api/v1/'
+    public api_token: string = 'OTscjZ19F';
 }
